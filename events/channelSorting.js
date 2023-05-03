c5ba3794c69cc95a6e3550766aa52b14913597bb
Mass0070 <52866257+Mass0070@users.noreply.github.com>
2023-05-03 17:18:57 +0200

    Added a way to disable channelSorting

--- events/channelSorting.js
+++ events/channelSorting.js
@@ -1,10 +1,17 @@
 const { mongodb, privatrum } = require('../config.json');
 const dbPromise = require('../Utils/mongo.js');
 
+let enabled = false;
+
+
 module.exports = {
 	name: 'channelCreate',
 	once: false,
 	async execute(channel) {
+        if (!enabled) {
+            console.log('Module is disabled.');
+            return;
+        }
         if (channel?.parentId !== privatrum.kategori) {
             return;
         }
@@ -14,31 +21,39 @@ module.exports = {
 
         try {
             const rooms = await dbclient.db("SA-2").collection('privateRooms').find().toArray();
-        
+    
             // Sort the rooms array in ascending order of position
-            rooms.sort((a, b) => a.position - b.position);
-        
+            rooms.sort((a, b) => {
+                if (a.position !== b.position) {
+                    return a.position - b.position;
+                }
+                return a.name.localeCompare(b.name);
+            });
+            
+
             // Loop through each room
             for (let i = 0; i < rooms.length; i++) {
                 const room = rooms[i];
         
                 // Find the main room and waiting room channels
                 try {
-                    const mainRoomGuild = await client.channels.fetch(room.mainRoomID);
-                    const waitRoomGuild = await client.channels.fetch(room.waitingRoomID);
-        
+                    const guild = await client.guilds.fetch(room.guildID);
+                    const mainRoomGuild = guild.channels.cache.get(room.mainRoomID);
+                    const waitRoomGuild = guild.channels.cache.get(room.waitingRoomID);
+            
                     // Check if the main room is below the waiting room in position
                     //console.log("Before: " + mainRoomGuild.position + " - " + waitRoomGuild.position)
                     if (waitRoomGuild.position > mainRoomGuild.position) {
                         //console.log(mainRoomGuild.position + " - " + waitRoomGuild.position)
                         // Swap the positions of the two channels
-                        const tempPos = await mainRoomGuild.position;
-                        await mainRoomGuild.setPosition(tempPos);
-                        await waitRoomGuild.setPosition(tempPos + 1);
+                        const tempPos = mainRoomGuild.position;
+                        await mainRoomGuild.setPosition(waitRoomGuild.position);
+                        await waitRoomGuild.setPosition(tempPos);
                     }
-                } catch (error) {}
-            }
-        } catch (error) {
+                    } catch (error) {
+                    }
+                }
+            } catch (error) {
         }
-    }
+    }      
 }
